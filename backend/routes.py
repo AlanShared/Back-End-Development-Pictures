b9c70467a98364f143c421ec1e4eded4375d59b2
AlanShared <slink.popper@gmail.com>
2023-11-30 21:27:34 -0500

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return data
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,12 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    if id is None:
+        return jsonify({"error": "ID not provided"}), 400
+    for picture in data:
+        if picture['id'] == id:
+            return picture
+    return jsonify({"Message":"ID not found"}), 404
 
 
 ######################################################################
@@ -52,20 +57,36 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_pic = request.json
+    for photo in data:
+        if new_pic["id"] == photo['id']:
+            return {"Message": f"picture with id {new_pic['id']} already present"}, 302
+    data.append(new_pic)
+    return jsonify(new_pic), 201
 
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
-
-
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    updated_pic = request.json
+    for photo in data:
+        if photo['id'] == id:
+            photo["pic_url"] = updated_pic.get("pic_url", photo["pic_url"])
+            photo["event_country"] = updated_pic.get("event_country", photo["event_country"])
+            photo["event_state"] = updated_pic.get("event_state", photo["event_state"])
+            photo["event_city"] = updated_pic.get("event_city", photo["event_city"])
+            photo["event_date"] = updated_pic.get("event_date", photo["event_date"])
+            return {"message": f"Picture with ID {id} updated successfully"}, 200
+    return {"message": "picture not found"}, 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    for photo in data:
+        if photo['id'] == id:
+            data.remove(photo)
+            return {}, 204
+    return {"message": "picture not found"}, 404
